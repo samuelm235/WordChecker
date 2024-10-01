d68b823ce29870448ef3812f544539f336838e6a
samuelm235 <108957962+samuelm235@users.noreply.github.com>
2024-10-01 09:32:52 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,12 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        WordChecker test = new WordChecker();
+        String[] words = {"at", "cat", "catch", "catchat", "catchatat"};
+        test.add(words);
+        System.out.println(test.getWordList());
+        System.out.println(test.isWordChain());
+    }
+}
 
