e5a5e8e02066f17a730ff225a608e129e9655a6e
samuelm235 <108957962+samuelm235@users.noreply.github.com>
2024-10-16 09:27:05 -0400

    Added test cases

--- Main.java
+++ Main.java
@@ -1,13 +1,36 @@
+import java.util.ArrayList;
+
 public class Main
 {
     public static void main(String[] args)
     {
         ArrayList<String> words = new ArrayList<String>();
-        words.add("an");
-        words.add("band");
-        words.add("band");
-        words.add("abandon");
-        System.out.println(words);
+        ArrayList<String> words_again = new ArrayList<String>();
+        ArrayList<String> words_again_again = new ArrayList<String>();
+        words.add("catch");
+        words.add("bobcat");
+        words.add("catchacat");
+        words.add("cat");
+        words.add("at");
+        words_again.add("an");
+        words_again.add("band");
+        words_again.add("band");
+        words_again.add("abandon");
+        words_again_again.add("to");
+        words_again_again.add("too");
+        words_again_again.add("stool");
+        words_again_again.add("tools");
+        WordChecker x = new WordChecker(words);
+        WordChecker y = new WordChecker(words_again);
+        WordChecker z = new WordChecker(words_again_again);
+        System.out.println(y.getWordList());
+        System.out.println(y.isWordChain());
+        System.out.println(z.getWordList());
+        System.out.println(z.isWordChain());
+        System.out.println(x.getWordList());
+        System.out.println(x.isWordChain());
+        System.out.println(x.createList("cat"));
+        System.out.println(x.createList("catch"));
+        System.out.println(x.createList("dog"));
     }
 }
-
