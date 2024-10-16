372d574f14f1a2da737cd074955ba26fe82e981f
samuelm235 <108957962+samuelm235@users.noreply.github.com>
2024-10-16 09:27:53 -0400

    added createList

--- WordChecker.java
+++ WordChecker.java
@@ -13,7 +13,6 @@ public class WordChecker {
         wordList = list;
     }
 
-
     public void add(String[] words)
     {
         for(String word : words) wordList.add(word);
@@ -24,13 +23,7 @@ public class WordChecker {
         return wordList;
     }
 
-    /**
-     * Returns true if each element of wordList (except the first) contains the
-     * previous
-     * element as a substring and returns false otherwise, as described in part (a)
-     * Precondition: wordList contains at least two elements.
-     * Postcondition: wordList is unchanged.
-     */
+
     public boolean isWordChain() 
     {
         for(int i = 1; i < wordList.size(); i ++)
@@ -45,16 +38,17 @@ public class WordChecker {
             
             
     }
-
-    /**
-     * Returns an ArrayList<String> based on strings from wordList that start
-     * with target, as described in part (b). Each element of the returned ArrayList
-     * has had
-     * the initial occurrence of target removed.
-     * Postconditions: wordList is unchanged.
-     * Items appear in the returned list in the same order as they appear in
-     * wordList.
-     */
-    public ArrayList<String> createList(String target) {
-        /* to be implemented in part (b) */ }
+    public ArrayList<String> createList(String target) 
+    {
+        ArrayList<String> list = new ArrayList<String>();
+        for(String s: wordList)
+        {
+            if(s.indexOf(target) == 0)
+            {
+                list.add(s.substring(target.length()));
+            }
+        }
+        return list;
+    }
 }
+
